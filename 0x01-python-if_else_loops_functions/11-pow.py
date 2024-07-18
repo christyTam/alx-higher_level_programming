0f8e3fbb4d72f90ad843b218099bb10170194d40
christyTam <christyjames938@yahoo.com>
2024-07-18 03:02:53 +0000

    if else statement

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#raise to power
+
+def pow(a, b):
+	return a ** b
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#print lower case
+
+
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
